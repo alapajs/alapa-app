4c2b1d2bac15b50067ad9a875c9e69420d0cc2a4
michaelodeh <michael4dominion2@gmail.com>
2024-10-18 10:03:17 +0100

    View config Adjusted

--- src/config/view.ts
+++ src/config/view.ts
@@ -1,7 +1,6 @@
 import { ViewConfiguration } from "alapa";
 export const viewConfig: ViewConfiguration = {
   staticFilesPath: "static",
-  extensionWhitelist: [],
   dir: "views",
-  extension: "html",
+  extensions: "html",
 };
